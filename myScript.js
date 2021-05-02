d3f99a8754f2faa1f48eba38415e3056e9f66b18
F3licia <romeofelicia@gmail.com>
2021-05-02 23:55:10 +0200

    completo ma con errore in console

---
+++ myScript.js
@@ -0,0 +1,36 @@
+
+const app = new Vue(
+
+    {
+        el: '#vueContainer',
+        data: {
+            imgs:["imgs/1.jpg", "imgs/2.jpg", "imgs/3.jpg", "imgs/4.jpg"] ,
+            titles:["titolo-1", "titolo-2", "titolo-3", "titolo-4"],
+            activeImg: 0,
+            lastImg: 3,
+            classi:[],
+            },
+         methods:{
+                    forward() { 
+                        this.imgs = this.imgs.sort(); 
+                        this.activeImg = this.activeImg +1;
+                        if(this.activeImg >= this.imgs.length){
+                            this.activeImg = 0;
+                        }
+                        if(this.activeImg > this.imgs.length){
+                            this.activeImg = 0;
+                        }
+                    },
+                    back() {
+                        this.activeImg = this.activeImg -1;
+                        if(this.activeImg < 0){
+                            this.activeImg = this.lastImg;
+                        }
+                    },
+                }
+    } 
+);
+
+
+
+
